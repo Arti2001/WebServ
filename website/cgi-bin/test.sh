bf1bc0f8ee061adde141aaeadc5b525a0e793173
Paulius Minialga <pminialg@f0r4s26.codam.nl>
2025-07-10 11:37:40 +0200

    Improved the bash script to print variables on new lines

--- website/cgi-bin/test.sh
+++ website/cgi-bin/test.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-echo "Content-Type: text/plain"
+echo "Content-Type: text/html"
 echo ""
+echo "<pre>"
 echo "CGI Shell Script Test"
 echo "Current directory: $(pwd)"
 echo "Script name: $0"
+echo ""
 echo "Environment variables:"
 env | sort
+echo "</pre>"
